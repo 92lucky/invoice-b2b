b473a3676c94b2cfdf427ab80c8f3d218dd40abf
LuckyCode <yyussrraann1992@gmail.com>
2026-06-15 17:49:53 +0800

    back5

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,7 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
+    secureCookie: true,
   });
 
   const isProtected =
